7ed35ef2bb89290d015e9dec80b2092bd9d25974
Yuhan Lin <yuhanlin1105@gmail.com>
2018-01-08 16:10:58 -0500

    add icon

--- src/components/UI/FeatureBox/FeatureBox.js
+++ src/components/UI/FeatureBox/FeatureBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const featureBox = (props) => {
     return (
         <div className="feature-box">
-            <span className="feature-box__icon" style={{backgroundImage:"url(https://png.icons8.com/ultraviolet/26/000000/geography.png)"}}/>
+            <i className="fa fa-globe feature-box__icon"/>
             <h3 className="heading-tertiary u-margin-bottom-small" >Explore the world</h3>
             <p className="feature-box__text">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, quibusdam facilis non distinctio et explicabo ea delectus ullam? Consequatur aliquid nobis accusantium debitis qui nam totam vel incidunt, assumenda repudiandae.
