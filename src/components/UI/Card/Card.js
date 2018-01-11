4c93eec01669ba760b6958758bdc3331b8e11da0
Yuhan Lin <yuhanlin1105@gmail.com>
2018-01-10 20:53:05 -0500

    add card function

--- src/components/UI/Card/Card.js
+++ src/components/UI/Card/Card.js
@@ -1,10 +1,46 @@
 import React from 'react';
 
+
 const card = (props) => {
     return (
-        <div>
-            {props.title}
+        <div className={props.style.card}>
+            <div className={props.style.frontSide.side}>
+                <div className={props.style.frontSide.img}>
+                    &nbsp;
+                </div>
+                <h4 className={props.style.frontSide.heading}>
+                    <span className={props.style.frontSide.headingSpan}>
+                        {props.frontSide.title}
+                    </span>
+
+                </h4>
+                <div className={props.style.frontSide.details}>
+                    <ul>
+                        <li> {props.frontSide.details.time}</li>
+                        <li> {props.frontSide.details.people}</li>
+                        <li> {props.frontSide.details.guide}</li>
+                        <li> {props.frontSide.details.stay}</li>
+                        <li> {props.frontSide.details.difficulty}</li>
+                    </ul>
+                </div>
+            </div>
+            <div className={props.style.backSide.side}>
+                <div className={props.style.backSide.cta}>
+                    <div className={props.style.backSide.priceBox}> 
+                        <p className={props.style.backSide.priceOnly}>
+                            {props.backSide.priceOnly}
+                        </p>
+                        <p className={props.style.backSide.priceValue}>
+                            {props.backSide.priceValue}
+                        </p>
+                    </div>
+                    <a href="#" className={props.style.backSide.button}>
+                        {props.backSide.priceButton}
+                    </a>
+                </div>
+            </div>
         </div>
+
     );
 };
 
--- src/containers/NatourHome/Tours/Tours.js
+++ src/containers/NatourHome/Tours/Tours.js
@@ -2,27 +2,123 @@ import React from 'react';
 import Card from '../../../components/UI/Card/Card';
 
 const tours = (props) => {
-    const toursContent={
-        title:"most popular tours",
-        cardsContent:{
+    const toursContent = {
+        title: "most popular tours",
+        cardsContent: {
             c1: {
-                title: "EXPLORE THE WORLD",
-                content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.",
-                iconClass: "fa fa-globe",
+                frontSide: {
+                    title: "EXPLORE THE WORLD",
+                    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.",
+                    details: {
+                        time: "3 day tours",
+                        people: "Up to 30 people",
+                        guide: "2 tour guides",
+                        stay: "Sleep in cozy hotels",
+                        difficulty: "Difficulty:easy"
+                    }
+                },
+                backSide: {
+                    priceOnly: "Only",
+                    priceValue: "$200",
+                    priceButton:"Book now!"
+                },
+                style: {
+                    card: "card",
+                    frontSide: {
+                        side: "card__side card__side--front",
+                        img: "card__picture card__picture-1",
+                        heading: "card__heading",
+                        headingSpan: "card__heading-span card__heading-span--1",
+                        details: "card__details"
+
+                    },
+                    backSide: {
+                        side: "card__side card__side--back card__side--back-1",
+                        cta: "card__cta",
+                        priceBox: "card__price-box",
+                        priceOnly: "card__price-only",
+                        priceValue: "card__price-value",
+                        button: "btn btn--white"
+                    }
+                }
             },
             c2: {
-                title: "MEET NATURE",
-                content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.",
-                iconClass: "fa fa-compass"
-    
+                frontSide: {
+                    title: "EXPLORE THE WORLD",
+                    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.",
+                    details: {
+                        time: "3 day tours",
+                        people: "Up to 30 people",
+                        guide: "2 tour guides",
+                        stay: "Sleep in cozy hotels",
+                        difficulty: "Difficulty:easy"
+                    }
+                },
+                backSide: {
+                    priceOnly: "Only",
+                    priceValue: "$200",
+                    priceButton:"Book now!"
+                },
+                style: {
+                    card: "card",
+                    frontSide: {
+                        side: "card__side card__side--front",
+                        img: "card__picture card__picture-2",
+                        heading: "card__heading",
+                        headingSpan: "card__heading-span card__heading-span--2",
+                        details: "card__details"
+
+                    },
+                    backSide: {
+                        side: "card__side card__side--back card__side--back-2",
+                        cta: "card__cta",
+                        priceBox: "card__price-box",
+                        priceOnly: "card__price-only",
+                        priceValue: "card__price-value",
+                        button: "btn btn--white"
+                    }
+                }
+
             },
             c3: {
-                title: "FIND YOUR WAY",
-                content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.",
-                iconClass: "fa fa-map-o"
-    
+                frontSide: {
+                    title: "EXPLORE THE WORLD",
+                    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.",
+                    details: {
+                        time: "3 day tours",
+                        people: "Up to 30 people",
+                        guide: "2 tour guides",
+                        stay: "Sleep in cozy hotels",
+                        difficulty: "Difficulty:easy"
+                    }
+                },
+                backSide: {
+                    priceOnly: "Only",
+                    priceValue: "$200",
+                    priceButton:"Book now!"
+                },
+                style: {
+                    card: "card",
+                    frontSide: {
+                        side: "card__side card__side--front",
+                        img: "card__picture card__picture-3",
+                        heading: "card__heading",
+                        headingSpan: "card__heading-span card__heading-span--3",
+                        details: "card__details"
+
+                    },
+                    backSide: {
+                        side: "card__side card__side--back card__side--back-3",
+                        cta: "card__cta",
+                        priceBox: "card__price-box",
+                        priceOnly: "card__price-only",
+                        priceValue: "card__price-value",
+                        button: "btn btn--white"
+                    }
+                }
+
             }
-    
+
         }
     }
 
@@ -37,14 +133,19 @@ const tours = (props) => {
 
 
     return (
-        <div class="section-tours">
+        <div className="section-tours">
             <div className="u-center-text u-margin-bottom-big">
                 <h2 className="heading-secondary">
                     {toursContent.title}
                 </h2>
             </div>
-            <div class="row">
+            <div className="row">
                 {Cards}
+            </div>
+            <div className="u-center-text u-margin-top-big">
+                <a href="#" className="btn btn--green">
+                    DISCOVER ALL TOURS
+                </a>
             </div>
         </div>
     );
